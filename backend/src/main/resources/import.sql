2ebaf20bb8456955640dd1dd58ceb6953795d012
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-08 18:17:46 -0300

    build: seed database with users

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_user (first_name, last_name, email, password, created_at) VALUES ('John', 'Doe', 'john.doe@gmail.com', '$2a$12$POSAI1JsPBSGAKZAN8sWJ.mVETZPDtBSCxCnla73GiLOqrixGOaEy', NOW());
+INSERT INTO tb_user (first_name, last_name, email, password, created_at) VALUES ('Jane', 'Doe', 'jane.doe@gmail.com', '$2a$12$POSAI1JsPBSGAKZAN8sWJ.mVETZPDtBSCxCnla73GiLOqrixGOaEy', NOW());
+INSERT INTO tb_user (first_name, last_name, email, password, created_at) VALUES ('Mark', 'Doe', 'mark.doe@gmail.com', '$2a$12$POSAI1JsPBSGAKZAN8sWJ.mVETZPDtBSCxCnla73GiLOqrixGOaEy', NOW());
+INSERT INTO tb_user (first_name, last_name, email, password, created_at) VALUES ('Elise', 'Doe', 'elise.doe@gmail.com', '$2a$12$POSAI1JsPBSGAKZAN8sWJ.mVETZPDtBSCxCnla73GiLOqrixGOaEy', NOW());
