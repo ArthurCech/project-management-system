4a05431804a1aeead2ec57506ae597466f15b218
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-08 15:27:51 -0300

    create backend

---
+++ backend/src/main/java/com/example/makalu/MakaluApplication.java
@@ -0,0 +1,13 @@
+package com.example.makalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakaluApplication.class, args);
+	}
+
+}
